eebe275c037a7c479a2ca793adcfe67da196832d
dylanYim <dongkyun94@gmail.com>
2024-06-06 21:29:44 +0900

    fix : fix board entity @Column 추가 기타 lombok 추가

--- src/main/java/jungle/dylan/api/domain/board/Board.java
+++ src/main/java/jungle/dylan/api/domain/board/Board.java
@@ -4,14 +4,20 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Board {
 
     @Id
     @GeneratedValue
+    @Column(name = "board_id")
     Long id;
     String userName;
     String title;
