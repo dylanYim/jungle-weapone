c44fc7799a5c7545e5f3ab7b1148483a2c26049e
dylanYim <dongkyun94@gmail.com>
2024-06-06 21:31:23 +0900

    feat : implement BoardRepository.java

---
+++ src/main/java/jungle/dylan/api/repository/BoardRepository.java
@@ -0,0 +1,13 @@
+package jungle.dylan.api.repository;
+
+import jungle.dylan.api.domain.board.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findAllByOrderByCreateDate();
+}
