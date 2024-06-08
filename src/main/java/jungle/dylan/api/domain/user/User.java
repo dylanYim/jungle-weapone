75cae2c70c894a637523fde124d4eb8b06bbe354
dylanYim <dongkyun94@gmail.com>
2024-06-08 13:53:02 +0900

    feat : implement User Entity

---
+++ src/main/java/jungle/dylan/api/domain/user/User.java
@@ -0,0 +1,25 @@
+package jungle.dylan.api.domain.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    @Column(name = "user_id")
+    Long id;
+    @Column(unique = true)
+    String username;
+    String password;
+    LocalDateTime createDate;
+
+}
