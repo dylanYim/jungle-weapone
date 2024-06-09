ac909eedb523aaacba741df60455a742691c63cc
dylanYim <dongkyun94@gmail.com>
2024-06-09 11:03:05 +0900

    refactor : change field username to writer and mapping User

--- src/main/java/jungle/dylan/api/domain/board/Board.java
+++ src/main/java/jungle/dylan/api/domain/board/Board.java
@@ -1,9 +1,12 @@
 package jungle.dylan.api.domain.board;
 
 import jakarta.persistence.*;
+import jungle.dylan.api.domain.user.User;
 import lombok.*;
 
 import java.time.LocalDateTime;
+
+import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @Getter
@@ -16,7 +19,10 @@ public class Board {
     @GeneratedValue
     @Column(name = "board_id")
     private Long id;
-    private String username;
+    private String writer;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
     private String title;
     private String contents;
     private String password;
