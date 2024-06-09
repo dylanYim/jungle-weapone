9d74afeacb011f60646e95dc5d4b9c79861710c0
dylanYim <dongkyun94@gmail.com>
2024-06-09 11:07:11 +0900

    implement : implement CustomUserDetailsService for load user to spring security context

---
+++ src/main/java/jungle/dylan/api/service/impl/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package jungle.dylan.api.service.impl;
+
+import jungle.dylan.api.domain.user.User;
+import jungle.dylan.api.exception.UserNotFoundException;
+import jungle.dylan.api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username).orElseThrow(UserNotFoundException::new);
+        return org.springframework.security.core.userdetails.User
+                .withUsername(username)
+                .password(user.getPassword())
+                .authorities(user.getRole().toString())
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(false)
+                .build();
+    }
+}
