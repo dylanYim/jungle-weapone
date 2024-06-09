9dfc325904552083cc285b6e813e52da0663df02
dylanYim <dongkyun94@gmail.com>
2024-06-09 11:11:34 +0900

    implement : change password validation rule and add password encoder and role

--- src/main/java/jungle/dylan/api/dto/UserRequest.java
+++ src/main/java/jungle/dylan/api/dto/UserRequest.java
@@ -1,9 +1,11 @@
 package jungle.dylan.api.dto;
 
 import jakarta.validation.constraints.Pattern;
+import jungle.dylan.api.domain.user.Role;
 import jungle.dylan.api.domain.user.User;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDateTime;
 
@@ -13,13 +15,14 @@ import java.time.LocalDateTime;
 public class UserRequest {
     @Pattern(regexp = "^[a-z0-9]{4,10}$", message = "Username must be between 4 and 10 characters long and contain only lowercase letters and digits.")
     private String username;
-    @Pattern(regexp = "^[a-zA-Z0-9]{8,15}$", message = "Password must be between 8 and 15 characters long and contain only letters and digits.")
+    @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()]{8,15}$", message = "Password must be between 8 and 15 characters long and contain only letters and digits.")
     private String password;
 
-    public User createUser() {
+    public User createUser(PasswordEncoder passwordEncoder) {
         return User.builder()
                 .username(username)
-                .password(password)
+                .password(passwordEncoder.encode(password))
+                .role(Role.USER)
                 .createDate(LocalDateTime.now())
                 .build();
     }
