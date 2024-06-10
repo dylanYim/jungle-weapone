22d464b38065e519ca65615bbedc5d00ea8e6b6d
dylanYim <dongkyun94@gmail.com>
2024-06-10 11:20:27 +0900

    implement : add comment mapping

--- src/main/java/jungle/dylan/api/domain/board/Board.java
+++ src/main/java/jungle/dylan/api/domain/board/Board.java
@@ -1,10 +1,13 @@
 package jungle.dylan.api.domain.board;
 
 import jakarta.persistence.*;
+import jungle.dylan.api.domain.comment.Comment;
 import jungle.dylan.api.domain.user.User;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static jakarta.persistence.FetchType.LAZY;
 
@@ -25,6 +28,8 @@ public class Board {
     private User user;
     private String title;
     private String contents;
+    @OneToMany(mappedBy = "board",cascade = CascadeType.ALL)
+    private List<Comment> comments = new ArrayList<>();
     private String password;
     private LocalDateTime createDate;
 
