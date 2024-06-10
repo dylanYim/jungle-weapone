f1af82c07e7476f2a1476d8ced5e694ae262301a
dylanYim <dongkyun94@gmail.com>
2024-06-10 11:20:41 +0900

    implement : add comment list to response dto

--- src/main/java/jungle/dylan/api/dto/BoardResponse.java
+++ src/main/java/jungle/dylan/api/dto/BoardResponse.java
@@ -2,10 +2,15 @@ package jungle.dylan.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jungle.dylan.api.domain.board.Board;
+import jungle.dylan.api.domain.comment.Comment;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Builder
@@ -14,17 +19,26 @@ public class BoardResponse {
     String writer;
     String title;
     String contents;
+    List<CommentResponse> comments;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime createDate;
 
     public static BoardResponse of(Board board) {
+        List<Comment> comments = board.getComments();
+        List<CommentResponse> commentResponses = Collections.emptyList();
+        if (comments != null) {
+            commentResponses = comments.stream()
+                    .map(CommentResponse::of)
+                    .collect(Collectors.toList());
+        }
+
         return BoardResponse.builder()
                 .id(board.getId())
                 .writer(board.getWriter())
                 .title(board.getTitle())
                 .contents(board.getContents())
                 .createDate(board.getCreateDate())
+                .comments(commentResponses)
                 .build();
     }
-
 }
